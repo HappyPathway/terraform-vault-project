017a21219098f1b85d41c9453b37e3f6a69835e5
Dave Arnold <darnold@hashicorp.com>
2018-08-09 10:59:50 -0700

    removing vault variables, using env vars

--- provider.tf
+++ provider.tf
@@ -1,4 +1,1 @@
-provider "vault" {
-  address = "${var.vault_cluster}"
-  token   = "${var.vault_token}"
-}
+provider "vault" {}
--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-variable "vault_token" {
-    type = "string"
-    description = "Vault Token"
-}
-
-variable "vault_cluster" {
-  type = "string"
-  description = "Vault Cluster Address"
-}
-
 variable "project" {
   type = "string"
   description = "Vault Project Name"
